fe6d0b562197a9a4f7bb0e5129b3f9802faa85fa
Phonethedor <alvaroc.giovannim@gmail.com>
2022-04-11 16:56:29 -0400

    agregados comentarios

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+//inicializador de tooltips
+
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
